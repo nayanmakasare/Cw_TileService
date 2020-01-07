aaea47a30f55de3a9b04a5305e16b5549d830e61
nayan <nvmakasare@gmail.com>
2020-01-07 22:17:46 +0530

    first commit

---
+++ apihandler/handler.go
@@ -0,0 +1,253 @@
+package apihandler
+
+import (
+	pb "Cw_TileService/proto"
+	"context"
+	"fmt"
+	"github.com/go-redis/redis"
+	"github.com/golang/protobuf/proto"
+	"go.mongodb.org/mongo-driver/mongo"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"log"
+	"strings"
+	"sync"
+)
+
+type Server struct {
+	TileCollection *mongo.Collection
+	RedisConnection  *redis.Client
+}
+
+func(s *Server) checkInRedis(redisKey string) bool{
+	if s.RedisConnection.Exists(redisKey).Val() == 1 {
+		return  true
+	}else {
+		return  false
+	}
+}
+
+
+func(s *Server) CloudwalkerPrimePages(ctx context.Context, request *pb.PrimePagesRequest)(*pb.CloudwalkerSchedule, error){
+		redisKey := fmt.Sprintf("%s:%s:cloudwalkerPrimePages", strings.ToLower(request.GetVendor()), strings.ToLower(request.GetBrand()))
+		if s.checkInRedis(redisKey) {
+			result, err := s.RedisConnection.SMembers(redisKey).Result()
+			if err != nil {
+				return nil, status.Error(codes.Unavailable, fmt.Sprintf("Failed to get Result from Cache ", err))
+			}
+			var schedule pb.CloudwalkerSchedule
+			for _, value := range result {
+				var page pb.Page
+				//Important lesson, challenge was to convert interface{} to byte. used  ([]byte(k.(string)))
+				err = proto.Unmarshal([]byte(value), &page)
+				if err != nil {
+					return nil, status.Error(codes.Internal, fmt.Sprintf("Failed to unMarshal result ", err))
+				}
+				schedule.Pages = append(schedule.Pages, &page)
+			}
+			return &schedule, nil
+		}else{
+			return nil, status.Error(codes.DataLoss, fmt.Sprintf("Data not found on cache "))
+		}
+}
+
+func (s *Server) GetPage(ctx context.Context, request *pb.PageRequest)(*pb.PageResponse, error)  {
+	redisKey := fmt.Sprintf("%s:%s:%s", strings.ToLower(request.GetVendor()), strings.ToLower(request.GetBrand()), strings.ToLower(request.GetPageName()))
+	if s.checkInRedis(redisKey) {
+		result, err := s.RedisConnection.SMembers(redisKey).Result()
+		if err != nil {
+			return nil, status.Error(codes.Unavailable, fmt.Sprintf("Failed to get Result from Cache ", err))
+		}
+		var response pb.PageResponse
+		err = proto.Unmarshal([]byte(result[0]), &response)
+		if err != nil {
+			return nil, status.Error(codes.Internal, fmt.Sprintf("Failed to unMarshal result ", err))
+		}
+		return &response, nil
+	}else {
+		return nil, status.Error(codes.DataLoss, fmt.Sprintf("Data not found on cache "))
+	}
+}
+
+func (s *Server) GetCarousel(ctx context.Context, request *pb.CarouselRequest) (*pb.CarouselResponse, error) {
+
+	redisKey := fmt.Sprintf("%s:%s:%s:carousel", strings.ToLower(request.GetVendor()), strings.ToLower(request.GetBrand()), strings.ToLower(request.GetPageName()))
+	if s.checkInRedis(redisKey) {
+
+		result, err := s.RedisConnection.SMembers(redisKey).Result()
+		if err != nil {
+			return nil, status.Error(codes.Unavailable, fmt.Sprintf("Failed to get Result from Cache ", err))
+		}
+		var carouselResponse pb.CarouselResponse
+		carouselResponse.CarouselBaseUrl = "http://cloudwalker-assets-prod.s3.ap-south-1.amazonaws.com/images/tiles/"
+		for _, value := range result {
+			var response pb.Carousel
+			err = proto.Unmarshal([]byte(value), &response)
+			if err != nil {
+				return nil, status.Error(codes.Internal, fmt.Sprintf("Failed to unMarshal result ", err))
+			}
+			carouselResponse.Carousel = append(carouselResponse.Carousel, &response)
+		}
+		return &carouselResponse, nil
+	}else {
+		return nil, status.Error(codes.DataLoss, fmt.Sprintf("Data not found on cache "))
+	}
+}
+
+func (s *Server) GetRow(ctx context.Context,  request *pb.RowRequest) (*pb.RowResponse, error)  {
+
+	redisKey := fmt.Sprintf("%s:%s:%s:%s", strings.ToLower(request.GetVendor()), strings.ToLower(request.GetBrand()), strings.ToLower(request.GetPageName()), strings.ToLower(request.GetRowName()))
+	if s.checkInRedis(redisKey) {
+		var interResp pb.InterRowResponse
+		result, err := s.RedisConnection.SMembers(redisKey).Result()
+		if err != nil {
+			return nil, status.Error(codes.Unavailable, fmt.Sprintf("Failed to get Result from Cache ", err))
+		}
+
+		err = proto.Unmarshal([]byte(result[0]), &interResp)
+		if err != nil {
+			return nil, status.Error(codes.Internal, fmt.Sprintf("Failed to unMarshal result ", err))
+		}
+		var response pb.RowResponse
+		response.RowName = interResp.RowName
+		response.RowLayout = interResp.RowLayout
+		response.ContentBaseUrl = interResp.ContentBaseUrl
+
+		if s.checkInRedis(interResp.ContentId) {
+			var result []string
+			if interResp.Shuffle == true {
+				result, err = s.RedisConnection.SRandMemberN(interResp.ContentId, 15).Result()
+				if err != nil {
+					return nil, status.Error(codes.Unavailable, fmt.Sprintf("Failed to get Result from Cache ", err))
+				}
+			}else {
+				result, err = s.RedisConnection.SMembers(interResp.ContentId).Result()
+				if err != nil {
+					return nil, status.Error(codes.Unavailable, fmt.Sprintf("Failed to get Result from Cache ", err))
+				}
+			}
+
+
+			for i , v := range result {
+				if i < 15 {
+					var contentTile pb.ContentTile
+					err = proto.Unmarshal([]byte(v), &contentTile)
+					if err != nil {
+						return nil, status.Error(codes.Internal, fmt.Sprintf("Failed to unMarshal result ", err))
+					}
+					response.ContentTiles = append(response.ContentTiles, &contentTile)
+				}
+			}
+		}
+		return &response, nil
+
+	}else {
+		return nil, status.Error(codes.DataLoss, fmt.Sprintf("Data not found on cache "))
+	}
+
+
+
+	//log.Println("getRows 1   "+req.RowId)
+	//var rowResp pb.RowResponse
+	//
+	//infoSet := strings.Split(req.RowId, "-")
+	//
+	//rowResp.RowName = strings.Replace(infoSet[1], "_", " ", -1)
+	//
+	//result, err := s.RedisConnection.SMembers(fmt.Sprintf("%s-%s-rowLayout",infoSet[0], infoSet[1])).Result()
+	//if err != nil {
+	//	return nil, err
+	//}
+	//rowResp.RowLayout = result[0]
+	//
+	//baseUrlResult, err := s.RedisConnection.SMembers(fmt.Sprintf("%s-row-baseurl", infoSet[0])).Result()
+	//if err != nil {
+	//	log.Println("getRows 4")
+	//	return nil, err
+	//}
+	//rowResp.ContentBaseUrl = baseUrlResult[0]
+	//
+	//
+	//var nextCursor uint64
+	//var wg sync.WaitGroup
+	//for {
+	//	//result, serverCursor, err := s.RedisConnection.SScan(req.RowId, nextCursor, "", chunkDataCount).Result()
+	//	result, serverCursor, err := s.RedisConnection.SScan(req.RowId, nextCursor, "", 300).Result()
+	//	if err != nil {
+	//		log.Println("getRows 5")
+	//		return nil, err
+	//	}
+	//	nextCursor = serverCursor
+	//	wg.Add(1)
+	//
+	//	go func(redisConn *redis.Client) error {
+	//
+	//		for _, k := range result {
+	//			//Important lesson, challenge was to convert interface{} to byte. used  ([]byte(k.(string)))
+	//			var movieTile pb.ContentTile
+	//			err = proto.UnmarshalText(k, &movieTile)
+	//			//err = proto.Unmarshal(([]byte(k.(string))), &movieTile)
+	//			if err != nil {
+	//				log.Println("getRows 6")
+	//				return err
+	//			}
+	//			rowResp.ContentTile = append(rowResp.ContentTile, &movieTile)
+	//		}
+	//		wg.Done()
+	//		return nil
+	//	}(s.RedisConnection)
+	//
+	//	wg.Wait()
+	//	if serverCursor == 0 {
+	//		break
+	//	}
+	//}
+	//return &rowResp, nil
+	//
+	//return  nil, nil
+}
+
+
+func(s *Server) GetContent(request *pb.RowRequest, stream pb.TileService_GetContentServer) error {
+	redisKey := fmt.Sprintf("%s:%s:%s:%s:content", strings.ToLower(request.GetVendor()), strings.ToLower(request.GetBrand()), strings.ToLower(request.GetPageName()), strings.ToLower(request.GetRowName()))
+	if s.checkInRedis(redisKey) {
+		var nextCursor uint64
+		var wg sync.WaitGroup
+		for {
+			//result, serverCursor, err := s.RedisConnection.SScan(req.RowId, nextCursor, "", chunkDataCount).Result()
+			result, serverCursor, err := s.RedisConnection.SScan(redisKey, nextCursor, "", 300).Result()
+			if err != nil {
+				log.Println("getRows 5")
+				return status.Error(codes.Internal, fmt.Sprintf("Cache parsing error ", err))
+			}
+			nextCursor = serverCursor
+			wg.Add(1)
+
+			go func(redisConn *redis.Client) error {
+
+				for _, k := range result {
+					//Important lesson, challenge was to convert interface{} to byte. used  ([]byte(k.(string)))
+					var movieTile pb.ContentTile
+					err = proto.Unmarshal([]byte(k), &movieTile)
+					if err != nil {
+						return status.Error(codes.Internal, fmt.Sprintf("Failed to unMarshal result ", err))
+					}
+					err = stream.Send(&movieTile)
+					if err != nil {
+						return status.Error(codes.Internal, fmt.Sprintf("Stream sending error ", err))
+					}
+				}
+				wg.Done()
+				return nil
+			}(s.RedisConnection)
+
+			wg.Wait()
+			if serverCursor == 0 {
+				break
+			}
+		}
+		return nil;
+	}else {
+		return status.Error(codes.DataLoss, fmt.Sprintf("Data not found on cache "))
+	}
+}
---
+++ client/main.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	pb "TileService/proto"
+	"context"
+	"google.golang.org/grpc"
+	"log"
+	"sync"
+)
+
+type Authentication struct {
+	Login string
+	Password string
+}
+
+func (a *Authentication) GetRequestMetadata(context.Context, ...string) (map[string]string , error){
+	return map[string]string{
+		"login": a.Login,
+		"password": a.Password,
+	}, nil
+}
+
+// RequireTransportSecurity indicates whether the credentials requires transport security
+func (a *Authentication) RequireTransportSecurity() bool {
+	return true
+}
+
+
+func makingConnection() (*grpc.ClientConn, error){
+	// creating creds for grpc client connection
+	//creds, err := credentials.NewClientTLSFromFile("cert/server.crt", "")
+	//if err != nil {
+	//	log.Fatalf("could not load tls cert: %s", err)
+	//}
+	//
+	//auth := Authentication{
+	//	Login:    "nayan",
+	//	Password: "makasare",
+	//}
+
+	//conn , err := grpc.Dial("cloudwalker.services.tv:7771", grpc.WithTransportCredentials(creds), grpc.WithPerRPCCredentials(&auth))
+	conn , err := grpc.Dial("192.168.1.143:7773", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %s", err)
+	}
+	return conn, err
+}
+
+func main(){
+	// stress testing run the rpc for how many times
+	run_test_index := 1;
+
+	var wg sync.WaitGroup
+	conn, err := makingConnection()
+	if err != nil {
+		log.Fatal(err)
+	}
+	c :=  pb.NewTileServiceClient(conn)
+
+
+	wg.Add(run_test_index)
+	for i:=0 ; i < run_test_index ; i++ {
+		//go CloudwalkerPrimePages(c, &wg)
+		//go GetPage(c, &wg)
+		//go GetCarousel(c, &wg)
+		go GetRow(c, &wg)
+	}
+	wg.Wait()
+}
+
+
+func CloudwalkerPrimePages(c pb.TileServiceClient, wg *sync.WaitGroup){
+	resp, err := c.CloudwalkerPrimePages(context.Background(), &pb.PrimePagesRequest{
+		Vendor:               "cvte",
+		Brand:                "shinko",
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(resp.Pages)
+	wg.Done()
+}
+
+func GetPage(c pb.TileServiceClient, wg *sync.WaitGroup){
+	resp, err := c.GetPage(context.Background(), &pb.PageRequest{
+		Vendor:               "cvte",
+		Brand:                "shinko",
+		PageName:             "movies",
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(resp)
+	wg.Done()
+}
+
+func GetCarousel(c pb.TileServiceClient, wg *sync.WaitGroup){
+	resp, err := c.GetCarousel(context.Background(), &pb.CarouselRequest{
+		Vendor:               "cvte",
+		Brand:                "shinko",
+		PageName:             "movies",
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(resp)
+	wg.Done()
+}
+
+func GetRow(c pb.TileServiceClient, wg *sync.WaitGroup){
+	resp, err := c.GetRow(context.Background(), &pb.RowRequest{
+		Vendor:               "cvte",
+		Brand:                "shinko",
+		PageName:             "movies",
+		RowName:              "hindi_dub_movies",
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(resp)
+	wg.Done()
+}
+
